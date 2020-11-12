e04d3303675df022f0db834fb4881b6bdb037b66
unknown <perekopskiy@dlit.dp.ua>
2020-11-12 18:12:32 +0200

    feat: helabet parse

--- bets/MainForm.Designer.cs
+++ bets/MainForm.Designer.cs
@@ -29,13 +29,37 @@ namespace bets
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(125, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(386, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(402, 340);
+            this.listBox1.TabIndex = 1;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.ResumeLayout(false);
@@ -43,6 +67,9 @@ namespace bets
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- bets/MainForm.cs
+++ bets/MainForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using bets.Data;
+using bet.Functions;
 
 namespace bets
 {
@@ -15,6 +17,28 @@ namespace bets
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task task3 = Task.Run(Helabet.Start);
+            task3.Wait();
+            List<Bookmaker> bookmakers = new List<Bookmaker>();
+            bookmakers.Add(Helabet.GetHelabet());
+            foreach (Bookmaker bookmaker in bookmakers)
+            {
+                listBox1.Items.Add(bookmaker.Name);
+                listBox1.Items.Add(bookmaker.ListOfMatches.Count);
+                foreach (Match match in bookmaker.ListOfMatches)
+                {
+                    match.MatchName = match.MatchName.Replace("Gaming", "").Replace("Academy", "").Replace("Esports", "").Replace("eSports", "");
+                    listBox1.Items.Add(match.MatchName + " " + match.DateTime.ToString());
+                    foreach (Bet bet in match.ListOfBets)
+                    {
+                        listBox1.Items.Add(bet.Name + " " + bet.Coef);
+                    }
+                }
+            }
         }
     }
 }
